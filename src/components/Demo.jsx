d395b8363a4e16de5ff89335cef18ba1f256500a
kodaliakhil <kodaliakhil1999@gmail.com>
2024-05-03 23:01:49 +0530

    fetching ariticle summary based on form submit feature

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
-
-  const handleSubmit = () => {
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     //Fetch Summary
-    alert("submitted");
+    const data = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
   };
   return (
     <section className="mt-16 w-full max-w-xl">
--- src/services/article.js
+++ src/services/article.js
@@ -5,8 +5,8 @@ export const articleApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", "KEY");
-      //   headers.set("X-RapidAPI-Key", rapidApiKey);
+    //   headers.set("X-RapidAPI-Key", "KEY");
+        headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
