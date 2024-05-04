a12513fad2657f2f5fb2023cebb2c0db1044e462
kodaliakhil <kodaliakhil1999@gmail.com>
2024-05-04 16:24:49 +0530

    env variables changes for deployment

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const rapidApiKey = import.meta.env.VITE_RAPID_KEY_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_TOKEN;
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
